ce86069d6b114092083dcc93b3dc9e6c5996d9c3
gao <gaomingqiao@gmail.com>
2023-11-15 15:13:55 +0100

    redux

--- components/Home.js
+++ components/Home.js
@@ -1,21 +1,28 @@
-import React from 'react'
-import styles from '../styles/Home.module.css';
-import Menu from './Menu'
-import Article from './Article';
-import Footer from './Footer';
+import React from "react";
+import styles from "../styles/Home.module.css";
+import Menu from "./Menu";
+import Article from "./Article";
+import Footer from "./Footer";
+import Image from "next/image";
+import { useSelector, useDispatch } from "react-redux";
+import { listNews } from "@/reducers/news";
 
 export default function Home() {
-    return (
-        <div className={styles.main}>
-            <div>
-                <Menu></Menu>
-            </div>
-            <div className={styles.container}>
-                <Article></Article>
-            </div>
-            <div>
-                <Footer></Footer>
-            </div>
-        </div>
-    )
+  const dispatch = useDispatch();
+  const news = useSelector((state) => state.news.value);
+  console.log(news);
+
+  return (
+    <div className={styles.main}>
+      <div>
+        <Menu></Menu>
+      </div>
+      <div className={styles.container}>
+        <Article></Article>
+      </div>
+      <div>
+        <Footer></Footer>
+      </div>
+    </div>
+  );
 }
