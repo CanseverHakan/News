cfecb77f3416f086177e207c2dd968b070967668
Hakan Cansever <cansever.hakan94@gmail.com>
2023-11-15 11:58:38 +0100

    fav

--- components/Articles.js
+++ components/Articles.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import styles from '../styles/Articles.module.css'
-
+import { faBookmark } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export default function Articles() {
   return (
     <div className={styles.card}>
         <img className={styles.image} src='/test.jpg'></img>
         <div>
-        <h3 className={styles.titles}>TITLE</h3>
+        <h3 className={styles.titles}>TITLE  
+        <FontAwesomeIcon icon={faBookmark}></FontAwesomeIcon></h3>
         </div>
         <span className={styles.author}>AUTHOR</span>
         <p className={styles.description}>Factbox-What you need to know about Ampere, Renault's EV unit</p>
