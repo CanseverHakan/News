e7274fb3968500e89a20a0f1326bfd2ee380aec9
Hakan Cansever <cansever.hakan94@gmail.com>
2023-11-15 11:33:33 +0100

    card

--- components/Articles.js
+++ components/Articles.js
@@ -5,10 +5,12 @@ import styles from '../styles/Articles.module.css'
 export default function Articles() {
   return (
     <div className={styles.card}>
-        <img className={styles.image}></img>
-        <h3>TITLE</h3>
-        
-
+        <img className={styles.image} src='/test.jpg'></img>
+        <div>
+        <h3 className={styles.titles}>TITLE</h3>
+        </div>
+        <span className={styles.author}>AUTHOR</span>
+        <p className={styles.description}>Factbox-What you need to know about Ampere, Renault's EV unit</p>
     </div>
   )
 }
--- components/Home.js
+++ components/Home.js
@@ -1,11 +1,25 @@
 import React from 'react'
-import Menu from './Menu'
 import styles from '../styles/Home.module.css';
+import Menu from './Menu'
+import Articles from './Articles';
 
 export default function Home() {
     return (
-        <div>
-            <Menu></Menu>
+        <div className={styles.main}>
+            <div>
+                <Menu></Menu>
+            </div>
+            <div className={styles.container}>
+                <Articles></Articles>
+                <Articles></Articles>
+                <Articles></Articles>
+                <Articles></Articles>
+                <Articles></Articles>
+                <Articles></Articles>
+                <Articles></Articles>
+                <Articles></Articles>
+                <Articles></Articles>
+            </div>
         </div>
     )
 }
